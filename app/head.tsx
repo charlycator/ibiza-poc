5c746d939902b7a34a22387c6acf82524504a6e0
charlycator <devcharly@pm.me>
2023-09-05 09:54:38 +0200

    feat: PWA first version

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,24 @@ export default function Head() {
   return (
     <>
       <title> HOZON | Spare change to crypto </title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      {/* <meta content="width=device-width, initial-scale=1" name="viewport" /> */}
+      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
       <meta name="description" content="HOZON spare change to crypto" />
+
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="HOZON" />
+      <meta name="description" content="HOZON" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-TileColor" content="#64748B" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#64748B" />
+
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+
       <link rel="icon" href="/favicon.ico" />
     </>
   );
--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching
+});
 const nextConfig = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
   env: {
     MENDABLE: "6ad46006-1b67-4bcb-8266-ef7ce508454c"
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA({
+  // other configs
+  nextConfig,
+  reactStrictMode: false
+});
